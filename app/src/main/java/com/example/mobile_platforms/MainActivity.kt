a93f6c98b6b7cd7de466ee084e64d2c1e3d71dff
Gardering <andrpills@gmail.com>
2023-04-15 22:48:46 +0300

    CheckConnect

--- app/src/main/java/com/example/mobile_platforms/MainActivity.kt
+++ app/src/main/java/com/example/mobile_platforms/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mobile_platforms
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 123123123123123
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
